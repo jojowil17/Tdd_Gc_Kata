1c50e3abe0d5a2102e603cd8e95e8e30cefea480
jojowil17 <jojowil32@gmail.com>
2015-09-01 19:10:33 -0400

    did test for array of length of 100. create greene_county function return array

---
+++ Gc_Kata_test.rb
@@ -0,0 +1,13 @@
+require_relative"Gc_Kata.rb"
+require"minitest/autorun"
+
+class GC_kata_test < Minitest::Test
+
+        def test_create_array_with_100_elements
+		      array = greene_county
+			  assert_equal(100, array.length)
+		end
+
+
+end
+
